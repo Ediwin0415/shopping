070e4faa1bd15e7ffaacf5bdbefcb222e9e5d355
Ediwin0415 <1319386949@qq.com>
2020-07-22 19:29:24 +0800

    operate

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login_box">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!--scoped 表示当前组件下有效-->
+<style lang="less" scoped>
+    .login_box{
+        height: 500px;
+        background-color: red;
+    }
+</style>
