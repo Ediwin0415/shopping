df327d98591c81a487cbf7a19e4a9fd7bbc471ff
Ediwin0415 <1319386949@qq.com>
2020-07-23 19:49:24 +0800

    login finish

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 为了以后实例都可以用，所以把它挂在实例上
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes:[
+  routes: [
     {
-      path:'/',
-      redirect:'/login'
+      path: '/',
+      redirect: '/login'
     },
     {
-      path:'/login',
-      component:Login
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // 用户直接访问登录页面 直接放行
+  if (to.path === '/login') return next()
+  // 判断是否登录
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login') // 没有登录 强制跳转到登录页
+  next()
 })
 
 export default router
